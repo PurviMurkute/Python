ebb6b60597034bf11683cdcd13726bf90a6bfe6a
PurviMurkute <purvimurkute51@gmail.com>
2026-07-23 23:47:36 +0530

    solved second largest find problem

--- List-array/secondLargest.py
+++ List-array/secondLargest.py
@@ -1,3 +1,14 @@
 list = [12, 21, 1, 67, 56, 899, 788, 90, 1000, 10000]
 
-second_largest = list[0]
+second_largest = list[-1]
+largest = list[0]
+
+for i in range(1, len(list)):
+    if list[i] > largest:
+        largest = list[i]
+
+for i in range(1, len(list)):
+    if list[i] > second_largest and list[i] < largest:
+        second_largest = list[i]
+
+print("The second largest element in the list is:", second_largest)
